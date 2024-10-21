c10221a3a6fe78a2b1cb88248636c39dbd3c2460
Luis Miguel Barrera Romero <lmbr@protonmail.com>
2024-10-21 17:25:51 +0200

    211024

---
+++ aplicaciones-java/src/main/java/com/luismibaro/proyectos_sobre_java/aplicaciones_java/App.java
@@ -0,0 +1,13 @@
+package com.luismibaro.proyectos_sobre_java.aplicaciones_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
